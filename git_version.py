c240e6a17f1ee2f7dad8032e1dfedd35090f37db
Andrew Austin <aaustin@redhat.com>
2020-06-29 18:45:17 -0500

    Fix git_version script for python 3.6

--- git_version.py
+++ git_version.py
@@ -4,7 +4,7 @@ Import("env")
 TEMPLATE = '''#define GIT_VERSION "NOGIT"'''
 GIT_CMD = ["/usr/bin/git", "describe", "--long", "--dirty"]
 
-git_proc = subprocess.run(GIT_CMD, capture_output=True)
+git_proc = subprocess.run(GIT_CMD, stdout=subprocess.PIPE)
 version = git_proc.stdout.decode().rstrip()
 with open('include/version.h', 'w') as header_file:
     header_file.write(TEMPLATE.replace('NOGIT', version))
